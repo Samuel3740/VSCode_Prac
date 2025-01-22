536cfd199545833709ff666f4568beb9121abc81
Samuel <roysangjun@gmail.com>
2025-01-22 15:02:18 +0900

    commit 4

--- C++/Baek/test/test2.cpp
+++ C++/Baek/test/test2.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int main(){
     int N;
-    int sum = 0;
-    string input;
+    float sum = 0;
+    float avg;
     
     cin >> N;
-    cin >> input;
-
+    
+    vector<int> arr(N);
+    vector<float> tmp(N);
+    
     for(int i = 0; i < N; ++i){
-        sum += input[i] - '0';  
+        cin >> arr[i];
     }
-
-    cout << sum << endl;
+    
+   float Max_Value = *max_element(arr.begin(), arr.end());
+    
+    for(int i = 0; i < N; ++i){
+        tmp[i] = arr[i] * 100 / Max_Value;
+        sum += tmp[i];
+    }
+    
+    cout.precision(6);    
+    cout << fixed;    
+    avg = sum / N;
+    cout << avg << endl;
 }
