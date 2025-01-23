0032a58f05b790ef93eda658bbbfc562c4a6ae17
Samuel <roysangjun@gmail.com>
2025-01-23 12:22:27 +0900

    commit 8

--- C++/Baek/Vocabulary_Study_1157/Vocabulary study Object.cpp
+++ C++/Baek/Vocabulary_Study_1157/Vocabulary study Object.cpp
@@ -1,50 +1,42 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <string>
 using namespace std;
 
-class Vocabulary_Study{
-private:
-    vector<int> frequency;  
-    string input;
+int main() {
+    int maxFreq = 0;
     int count = 0;
+    char result = '?';
 
-public:
-    Vocabulary_Study() : frequency(26, 0){} 
+    string input;
+    cin >> input;
+    vector<int> frequency(26, 0);
 
-    void InputData(){
-        cin >> input;
-        transform(input.begin(), input.end(), input.begin(), ::toupper); // 모두 대문자 변환환
+    for (int i = 0; i < input.size(); ++i){
+        if (input[i] >= 'a' && input[i] <= 'z'){
+            input[i] -= 'a' - 'A';
+        }
     }
 
-    // input에 알파벳에 대해 각각 몇 번씩 나왔는디 frequency 배열에 저장장 
-    char Analyze(){
-        for (char c : input){           // input 문자열의 각 문자를 하나씩 꺼내서 변수 c에 할당, 이를 루프 바디에서 처리
-            if (isalpha(c)){            // 알파벳 대문자는 1 반환. 알파벳 소문자는 2 반환, 알파벳이 아닌 문자는 0 반환
-                frequency[c - 'A']++;   // 아스키 코드상 A는 65 ,B는 66, ... Z는 90
-            }
+    for (char c : input){
+        if (c >= 'A' && c <= 'Z'){
+            frequency[c - 'A']++;
         }
-
-        int MaxFreq = 0;
-        int index = -1;
-
-        for (int i = 0; i < 26; ++i){
-            if (frequency[i] > MaxFreq){
-                MaxFreq = frequency[i];
-                index = i;
-                count = 1;
-            }
-            else if (frequency[i] == MaxFreq){
-                count++;
-            }
-        }
-        return (count > 1) ? '?' : static_cast<char>(index + 'A');
     }
-};
 
-int main(){
-    Vocabulary_Study study;
-    study.InputData();
-    cout << study.Analyze() << endl;
+    for (int i = 0; i < 26; ++i){
+        if (frequency[i] > maxFreq){
+            maxFreq = frequency[i];
+            result = i + 'A';
+            count = 1;
+        } 
+        else if (frequency[i] == maxFreq){
+            count++;
+        }
+    }
+
+    if (count > 1){
+        result = '?';
+    }
+    cout << result << endl;
 }
