b1131e32aca69823ab1fa7a9a9153301cd3dbad8
Samuel <roysangjun@gmail.com>
2025-01-23 12:16:32 +0900

    commit 7

--- C++/Baek/Vocabulary_Study_1157/Vocabulary study Object.cpp
+++ C++/Baek/Vocabulary_Study_1157/Vocabulary study Object.cpp
@@ -8,29 +8,34 @@ class Vocabulary_Study{
 private:
     vector<int> frequency;  
     string input;
+    int count = 0;
 
 public:
     Vocabulary_Study() : frequency(26, 0){} 
 
     void InputData(){
         cin >> input;
-        transform(input.begin(), input.end(), input.begin(), ::toupper); 
+        transform(input.begin(), input.end(), input.begin(), ::toupper); // 모두 대문자 변환환
     }
 
+    // input에 알파벳에 대해 각각 몇 번씩 나왔는디 frequency 배열에 저장장 
     char Analyze(){
-        for (char c : input){ //input 문자열의 각 문자를 하나씩 꺼내서 변수 c에 할당, 이를 루프 바디에서 처리
-            if (isalpha(c)) { // 알파벳 대문자는 1 반환. 알파벳 소문자는 2 반환, 알파벳이 아닌 문자는 0 반환
-                frequency[c - 'A']++;
+        for (char c : input){           // input 문자열의 각 문자를 하나씩 꺼내서 변수 c에 할당, 이를 루프 바디에서 처리
+            if (isalpha(c)){            // 알파벳 대문자는 1 반환. 알파벳 소문자는 2 반환, 알파벳이 아닌 문자는 0 반환
+                frequency[c - 'A']++;   // 아스키 코드상 A는 65 ,B는 66, ... Z는 90
             }
         }
 
-        int maxFreq = 0, count = 0, index = -1;
+        int MaxFreq = 0;
+        int index = -1;
+
         for (int i = 0; i < 26; ++i){
-            if (frequency[i] > maxFreq){
-                maxFreq = frequency[i];
+            if (frequency[i] > MaxFreq){
+                MaxFreq = frequency[i];
                 index = i;
                 count = 1;
-            } else if (frequency[i] == maxFreq){
+            }
+            else if (frequency[i] == MaxFreq){
                 count++;
             }
         }
