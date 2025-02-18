aec32c4fcbd2d8e1bb399c6e6662721b6055463b
Samuel <roysangjun@gmail.com>
2025-02-18 10:45:20 +0900

    commit 30

--- C++/Baek/Vertical_Reading_10798/Vertical reading.cpp
+++ C++/Baek/Vertical_Reading_10798/Vertical reading.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 int main(){
     string arr[5];
+    
     for(int i = 0; i < 5; ++i){
         cin >> arr[i];
     }
-    for (int i = 0; i < 15; ++i){
-        for (int j = 0; j < 5; ++j){
-            if (arr[j].length() > i){
-                cout << arr[j][i];
+    
+    for(int j = 0; j < 15; ++j){
+        for(int i = 0; i < 5; ++i){
+            if(arr[i].length() > j){
+                cout << arr[i][j]; // arr[i]라는 단어 내에서 j번째 문자에 접근하는 방법
             }
         }
     }
