6c8d292fddd4ce0ddc5e0b2f712f60365947ba8b
Samuel <roysangjun@gmail.com>
2025-06-04 21:06:17 +0900

    commit 135

---
+++ C++/Baek/Step-by-Step Learning/Brute Force/Decomposition_Sum_2231/Decomposition Sum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int digit_sum(int num){
+    int sum = 0;
+
+    while(num > 0){
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+int main(){
+    int N;
+
+    cin >> N;
+
+    for(int i = 1; i < N; i++){
+        if(i + digit_sum(i) == N){
+            cout << i << endl;  
+
+            return 0;
+        }
+    }
+
+    cout << 0 << endl;
+    return 0;
+}
