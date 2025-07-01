24b97ddf126087b8a5ceaee2ae2c7bd41a8687ca
Samuel <roysangjun@gmail.com>
2025-07-01 18:06:19 +0900

    commmit 157

---
+++ C++/Baek/etc/Input, Output, and Arithmetic Operations/Copyright_2914/Copyright.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, I, result;
+
+    cin >> A >> I;
+
+    result = A * (I - 1) + 1;
+
+    cout << result << endl;
+
+    return 0;
+}
