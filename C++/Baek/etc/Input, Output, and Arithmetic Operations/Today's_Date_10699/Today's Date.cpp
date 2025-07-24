a8ab4295e83073ebe1a291b2e7bf23494277ed73
Samuel <roysangjun@gmail.com>
2025-07-24 21:00:45 +0900

    commit 180

---
+++ C++/Baek/etc/Input, Output, and Arithmetic Operations/Today's_Date_10699/Today's Date.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+
+int main() {
+    auto now = chrono::system_clock::now();
+    
+    time_t time = chrono::system_clock::to_time_t(now);
+    
+    tm* local_time = localtime(&time);
+    
+    cout << put_time(local_time, "%Y-%m-%d") << endl;
+    
+    return 0;
+}
