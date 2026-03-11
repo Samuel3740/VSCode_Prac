3f5d5665850d18f64ab3307a27eca6e4c55a8fb4
Samuel3740 <roysangjun@gmail.com>
2026-03-11 13:04:50 +0900

    commit 416

--- C++/Baek/etc/Input, Output, and Arithmetic Operations/Cupcake_Party_24568/Cupcake Party.cpp
+++ C++/Baek/etc/Input, Output, and Arithmetic Operations/Cupcake_Party_24568/Cupcake Party.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
 
-    int R, S;
-    cin >> R >> S;  
+  int R, S;
+  cin >> R >> S;
 
-    cout << 8*R + 3*S - 28;
+  cout << 8 * R + 3 * S - 28;
 }
